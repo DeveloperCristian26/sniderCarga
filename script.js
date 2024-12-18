992859b4c974caadd7fe2ae2bf66942ef8be5d0e
Cristian <makengolee@gmail.com>
2024-12-18 13:57:53 -0500

    primer codigo

---
+++ script.js
@@ -0,0 +1,8 @@
+var contenedor = document.getElementById('contenedor_carga');
+
+window.onload = function(){
+
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0'; 
+    
+}
